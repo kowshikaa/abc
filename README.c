fe3f0a5c33e67b8e9649bd001adbf0b59871998b
kowshikaa <kowshimano@gmail.com>
2016-07-10 12:17:38 +0530

    Update and rename README.md to README.c

---
+++ README.c
@@ -0,0 +1,25 @@
+# abc
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,n,c,j,i;
+printf("Enter a value:");
+scanf("%d",&a);
+printf("Enter the n value");
+scanf("%d",&n);
+printf("Enter the pair:");
+scanf("%d",&c);
+for(j=0;j<2*c;j++);
+{
+for(i=1;i=2*n-a;i++)
+if(c!=i)
+{
+printf("Enter the pair",c);
+printf("Enter the for day",i++,c);
+}
+}
+printf("Enter the calculation",a);
+
+return 0;
+}
